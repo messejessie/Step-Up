e1c5ef6c646fd8253866093bcc31455c29944341
messejessie <messejessie77@gmail.com>
2019-04-28 16:25:14 -0400

    things showing to the page

--- step-up/src/App.js
+++ step-up/src/App.js
@@ -1,27 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, {Component} from 'react';
+//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+//import { Button } from 'reactstrap';
+import Example from "../src/component/Jumbotron"
+import MainNav from "../src/component/nav"
 
-function App() {
-  return (
-    <Router>
-    <div>
-      <ul>
-          <li>
-          <Link to="/signup">Sign Up</Link>
-        </li>
-        <li>
-          <Link to="/Profile">Topics</Link>
-        </li>
-      </ul>
 
-      <hr />
+class App extends Component {
+  render () {
+    return (
+      <div className="app">
+      <MainNav />
+      <Example />
+      </div>
+    )
+  }
 
-      <Route exact path="/" component={Home} />
-      <Route path="/signup" component={About} />
-      <Route path="/topics" component={Topics} />
-    </div>
-  </Router>
-  );
 }
+
+// function App() {
+//   return (
+
+//   // <div>hello world</div>
+//     <Router>
+//     {/* <div>
+//       <ul>
+//           <li>
+//           <Link to="/signup">Sign Up</Link>
+//         </li>
+//         <li>
+//           <Link to="/Profile">Topics</Link>
+//         </li>
+//       </ul>
+
+//       <hr /> */}
+
+//       <Route exact path="/" page={Home} />
+//       {/* <Route path="/signup" component={About} />
+//       <Route path="/topics" component={Topics} /> */}
+
+//   </Router>
+//   );
+// }
 
 export default App;
