5fda518c9c2c3a37adc546e58f5423c88f1b8b55
messejessie <messejessie77@gmail.com>
2019-04-29 19:56:40 -0400

    login modal form set up

--- step-up/src/Pages/LogIn.js
+++ step-up/src/Pages/LogIn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from 'reactstrap';
 
 class LogIn extends React.Component {
   constructor(props) {
@@ -24,11 +24,19 @@ class LogIn extends React.Component {
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}>Log In</ModalHeader>
           <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <Form inline>
+              <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                <Label for="exampleEmail" className="mr-sm-2">Email</Label>
+                <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
+              </FormGroup>
+              <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                <Label for="examplePassword" className="mr-sm-2">Password</Label>
+                <Input type="password" name="password" id="examplePassword" placeholder="don't tell!" />
+              </FormGroup>
+            </Form>
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            <Button>Submit</Button>
           </ModalFooter>
         </Modal>
       </div>
