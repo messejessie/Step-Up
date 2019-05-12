7ecbbf73cd22a690b1717beb35b3d30c92227264
messejessie <messejessie77@gmail.com>
2019-05-12 12:01:46 -0400

    attempting css

--- step-up/src/component/blogPageCard.js
+++ step-up/src/component/blogPageCard.js
@@ -7,34 +7,40 @@ import { Col, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const MainBlog = (props) => {
 
- 
+
 
   return (
     <div>
-      <Card>
+      <Card >
         <CardBody>
           <CardTitle>Dive into Happiness</CardTitle>
           <CardText>
             <Form>
               <FormGroup row>
                 <Label for="exampleText" sm={2}>Three things you are grateful for:</Label>
-                <Col sm={10}>
+                <Col sm={12}>
                   <Input type="textarea" name="gratitude" id="exampleText" onChange={props.handleChange} />
                 </Col>
               </FormGroup>
               <FormGroup row>
                 <Label for="exampleText" sm={2}>Daily Affirmations:</Label>
-                <Col sm={10}>
+                <Col sm={12}>
                   <Input type="textarea" name="affirmations" id="exampleText" onChange={props.handleChange} />
                 </Col>
                 <FormGroup row>
                   <Label for="exampleText" sm={2}>Today Goals:</Label>
-                  <Col sm={10}>
+                  <Col sm={12}>
                     <Input type="textarea" name="goals" id="exampleText" onChange={props.handleChange} />
                   </Col>
+                  {/* <FormGroup>
+                    <Label for="exampleText">Text Area</Label>
+                    <Col sm={12}>
+                    <Input type="textarea" name="text" id="exampleText" />
+                    </Col>
+                  </FormGroup> */}
                   <FormGroup row>
                     <Label for="exampleText" sm={2}>Finally whats on your mind?:</Label>
-                    <Col sm={10}>
+                    <Col sm={12} >
                       <Input type="textarea" name="freeform" id="exampleText" onChange={props.handleChange} />
                     </Col>
                   </FormGroup>
