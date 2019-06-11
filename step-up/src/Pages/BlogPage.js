62408aa41d4a5861d2317ebb33b7eb0626856cd0
messejessie <messejessie77@gmail.com>
2019-06-11 19:22:29 -0400

    working on blog page connections

--- step-up/src/Pages/BlogPage.js
+++ step-up/src/Pages/BlogPage.js
@@ -10,102 +10,104 @@ import { Input, TextArea, FormBtn } from "../component/Form";
 
 
 class BlogPage extends Component {
-    state = {
-        memberid: '',
-        freeform: '',
-        goals: '',
-        gratitude: '',
-        affirmations: ''
-    }
+  state = {
+    memberid: '',
+    freeform: '',
+    goals: '',
+    gratitude: '',
+    affirmations: ''
+  }
 
-    handleChange = event => {
-        const { name, value } = event.target
+  handleChange = event => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleBlog = event => {
+    console.log('this state', this.state);
+    //const blogData = { freeform: this.state.freeform, goals: this.state.goals, affirmations: this.state.affirmations, gratitude: this.state.gratitude };
+    console.log('');
+    const { freeform, goals, gratitude, affirmations } = this.state;
+    event.preventDefault();
+    const blogData = { freeform, goals, gratitude, affirmations }
+    const memberid = sessionStorage.getItem('memberid');
+    apiBlog.saveBlog(blogData, memberid)
+      .then(response => {
+        //sessionStorage.setItem('authenticated', true)
         this.setState({
-            [name]: value
-        });
-    }
+          memberid: response.data_id,
+        }, () => {
+          this.props.history.push({
+            pathname: '/profile',
+            state: { memberid: 'something' }
+          })
+        })
 
-    handleBlog = event => {
-        console.log('this state', this.state);
-        //const blogData = { freeform: this.state.freeform, goals: this.state.goals, affirmations: this.state.affirmations, gratitude: this.state.gratitude };
-        console.log('');
-        const { freeform, goals, gratitude, affirmations } = this.state;
-        event.preventDefault();
-        const blogData = {  freeform, goals, gratitude, affirmations }
-        const memberid = sessionStorage.getItem('memberid');
-        apiBlog.saveBlog(blogData, memberid)
-            .then(response => {
-                //sessionStorage.setItem('authenticated', true)
-                this.setState({
-                    memberid: response.data_id,
-                }, () => {
-                    this.props.history.push({
-                        pathname: '/profile',
-                        state: { memberid: 'something' }
-                    })
-                })
+        console.log(response)
+      });
+
+  }
+
+  render() {
+
+    console.log(this.props)
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <Jumbotron>
+              <h1>Breathe In...Breathe Out</h1>
+            </Jumbotron>
+            <form>
+              <Input
+                name="gratitude"
+                placeholder="Something Your Grateful For"
+              />
+              <Input
+                name="affirmations"
+                placeholder="Today's Affirmation"
+              />
+              <Input
+                name="goals"
+                placeholder="Today's Goal"
+              />
+              <TextArea
+                name="freeform"
+                placeholder="How was today?"
                 
-         console.log(response)
-            });
-  
-    }
+              />
+             <FormBtn  onClick={this.handleBlog}>
+               Submit
+             </FormBtn>
+            </form>
+          </Col>
+          <Col size="md-6 sm-12">
+            <Jumbotron>
+            </Jumbotron>
+            <List>
+              <ListItem>
 
-    render() {
-
-        console.log(this.props)
-        return (
-            <Container fluid>
-              <Row>
-                <Col size="md-6">
-                  <Jumbotron>
-                    <h1>Breathe In...Breathe Out</h1>
-                  </Jumbotron>
-                  <form>
-                    <Input
-                      name="gratitude"
-                      placeholder="Something Your Grateful For"
-                    />
-                      <Input
-                      name="affirmations"
-                      placeholder="Today's Affirmation"
-                    />
-                      <Input
-                      name="goals"
-                      placeholder="Today's Goal"
-                    />
-                    <TextArea 
-                    name="freeform" 
-                    placeholder="How was today?" 
-                    />
-      
-                  </form>
-                </Col>
-                <Col size="md-6 sm-12">
-                  <Jumbotron>
-                    <h1>Shown progress</h1>
-                  </Jumbotron>
-                  <List>
-                    <ListItem>
-      
-                    </ListItem>
-                  </List>
-                </Col>
-              </Row>
-            </Container>
-          )
-        // return (
-        //     <div>
-        //         <MainBlog
-        //             handleChange={this.handleChange}
-        //             handleBlog={this.handleBlog}
-        //             freeform={this.state.freeform}
-        //             goals={this.state.goals}
-        //             affirmations={this.state.affirmations}
-        //             gratitude={this.state.gratitude}
-        //         />
-        //     </div>
-        // )
-    }
+              </ListItem>
+            </List>
+          </Col>
+        </Row>
+      </Container>
+    )
+    // return (
+    //     <div>
+    //         <MainBlog
+    //             handleChange={this.handleChange}
+    //             handleBlog={this.handleBlog}
+    //             freeform={this.state.freeform}
+    //             goals={this.state.goals}
+    //             affirmations={this.state.affirmations}
+    //             gratitude={this.state.gratitude}
+    //         />
+    //     </div>
+    // )
+  }
 
 }
 
